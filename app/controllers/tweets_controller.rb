509fce0d86b2654584501e1e9326797fd1bcf8d5
Akira <autumn.no7@gmail.com>
2019-07-23 10:54:07 +0900

    ツイート詳細画面追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,12 +3,18 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
   def new
     @tweet = Tweet.new
   end
+
   def create
     Tweet.create(tweet_params)
     redirect_to tweets_path
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
   end
 
   private
